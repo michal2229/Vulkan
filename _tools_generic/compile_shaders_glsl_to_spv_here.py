ffa960e2809230938e38b31cda851b7ebb9b5d76
Michal B <michal2229@gmail.com>
2017-06-11 17:50:51 +0200

    _tools_generic/compile_shaders_glsl_to_spv_here.py cleanup

--- _tools_generic/compile_shaders_glsl_to_spv_here.py
+++ _tools_generic/compile_shaders_glsl_to_spv_here.py
@@ -3,8 +3,8 @@
 import pyshaderc
 import sys
 
-scriptname = sys.argv[0]
-shadertype = sys.argv[1]  # vert, frag...
+scriptname = str(sys.argv[0])
+shadertype = str(sys.argv[1])  # vert, frag...
 filelist   = [str(i) for i in sys.argv[2:]]
 
 for fil in filelist:
@@ -12,16 +12,4 @@ for fil in filelist:
     
     with open("{}.spv".format(fil), "wb") as file:
         file.write(spirv)
-    
-    
-    
-#    spirvFile = open("{}.spv".format(fil), "wb")
-#    spirvFile.write(spirv)
-    
 
-#spirv = pyshaderc.compile_file_into_spirv('./instancing.vert', 'vert')
-#newFile = open("./instancing.vert.spv", "wb")
-#newFile.write(spirv)
-
-#with open("foo.bin", "wb") as file:
-#  file.write(pack("<IIIII", *bytearray([120, 3, 255, 0, 100])))
